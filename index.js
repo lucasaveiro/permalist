8e66730a77b6e49a64aa96357f6a44bf4c8fe9b0
Lucas Aveiro <lucas.n.aveiro@gmail.com>
2025-01-23 13:03:18 -0300

    feat: complete CRUD functionality for items

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ const db = new pg.Client({
   database: "permalist",
   password: "19111991",
   port: 5432,
-});
+  });
+
 
 db.connect();
 
@@ -36,25 +37,38 @@ app.get("/", (req, res) => {
   getAllTasks().then(result => {
     items = result;
     console.log(items);
+
+    res.render("index.ejs", {
+      listTitle: "Today",
+      listItems: items,
+    });
+
   }).catch(error => {
     console.error("Error:", error);
-  });
-
-  res.render("index.ejs", {
-    listTitle: "Today",
-    listItems: items,
+    res.render("index.ejs", {
+      listTitle: "Today",
+      listItems: [],
+    });
   });
 });
 
-app.post("/add", (req, res) => {
-  const item = req.body.newItem;
-  items.push({ title: item });
+app.post("/add", async (req, res) => {
+  await db.query("INSERT INTO item (title) VALUES ($1) RETURNING *", [req.body.newItem]);
+  getAllTasks();
   res.redirect("/");
 });
 
-app.post("/edit", (req, res) => {});
+app.post("/edit", async (req, res) => {
+  await db.query("UPDATE item SET title = $1 WHERE id = $2", [req.body.updatedItemTitle, req.body.updatedItemId]);
+  getAllTasks();
+  res.redirect("/");
+});
 
-app.post("/delete", (req, res) => {});
+app.post("/delete", async (req, res) => {
+  await db.query("DELETE FROM item WHERE id = $1", [req.body.deleteItemId]);
+  getAllTasks();
+  res.redirect("/");
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
